d601753bf03ac0f3b7e2edb3428ff7979306b8f7
topsycreed <topsycreed@gmail.com>
2015-06-21 12:31:03 +0300

    Added tests of group creating
    This is a result of homework #1

---
+++ addressbook-selenium-test/src/com/example/tests/GroupCreationTests.java
@@ -0,0 +1,30 @@
+package com.example.tests;
+
+import org.testng.annotations.Test;
+
+public class GroupCreationTests extends TestBase {
+  @Test
+  public void testNonEmptyGroupCreation() throws Exception {
+	openMainPage();
+    gotoGroupsPage();
+    initGroupCreation();
+    GroupData group = new GroupData();
+    group.name = "group name 1";
+    group.header = "header 1";
+    group.footer = "footer 1";
+	fillGroupForm(group);
+    submitGroupCreation();
+    returnToGroupsPage();
+  }
+  
+  @Test
+  public void testEmptyGroupCreation() throws Exception {
+	openMainPage();
+    gotoGroupsPage();
+    initGroupCreation();
+    GroupData group = new GroupData("", "", "");
+	fillGroupForm(group);
+    submitGroupCreation();
+    returnToGroupsPage();
+  }
+}
